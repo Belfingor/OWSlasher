b4742789e23067c5c5438cdcb965e0c9f67fac12
Daniil Mamonov <daniilmamonov1337@gmail.com>
2025-05-24 14:46:37 +1200

    Adding some basic classes and enabling Enhanced input for a pawn class

--- Source/OWSlasher/DebugMacros.h
+++ Source/OWSlasher/DebugMacros.h
@@ -2,14 +2,24 @@
 #include "DrawDebugHelpers.h"
 
 #define DRAW_SPHERE(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Red, true);
+#define DRAW_SPHERE_SINGLEFRAME(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Red, false, -1.f);
 #define DRAW_LINE(Start, End) if (GetWorld()) DrawDebugLine(GetWorld(), Start, End, FColor::Red, true, -1.f, 0, 1.f);
+#define DRAW_LINE_SINGLEFRAME(Start, End) if (GetWorld()) DrawDebugLine(GetWorld(), Start, End, FColor::Red, false, -1.f, 0, 1.f);
 #define DRAW_POINT(Location) if (GetWorld()) DrawDebugPoint(GetWorld(), Location, 15.f, FColor::Green, true);
+#define DRAW_POINT_SINGLEFRAME(Location) if (GetWorld()) DrawDebugPoint(GetWorld(), Location, 15.f, FColor::Green, false, -1.f);
 #define DRAW_BOX(Location, Extent) if (GetWorld()) DrawDebugBox(GetWorld(), Location, Extent, FColor::Yellow, true, -1.f, 0, 1.f);
+#define DRAW_BOX_SINGLEFRAME(Location, Extent) if (GetWorld()) DrawDebugBox(GetWorld(), Location, Extent, FColor::Yellow, false, -1.f, 0, 1.f);
 //----------------------------------------------------------------------------- Draw a Vector with a point at its head
 #define DRAW_VECTOR(Start, End) if (GetWorld()) \
 	{ \
 		DrawDebugLine(GetWorld(), Start, End, FColor::Red, true, -1.f, 0, 1.f); \
 		DrawDebugPoint(GetWorld(), End, 15.f, FColor::Green, true); \
+	}
+
+#define DRAW_VECTOR_SINGLEFRAME(Start, End) if (GetWorld()) \
+	{ \
+		DrawDebugLine(GetWorld(), Start, End, FColor::Red, false, -1.f, 0, 1.f); \
+		DrawDebugPoint(GetWorld(), End, 15.f, FColor::Green, false, -1.f); \
 	}
 //-----------------------------------------------------------------------------
 
--- Source/OWSlasher/Private/Items/Item.cpp
+++ Source/OWSlasher/Private/Items/Item.cpp
@@ -12,26 +12,40 @@ AItem::AItem()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+	RootComponent = ItemMesh;
 }
 
 // Called when the game starts or when spawned
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-	UWorld* World = GetWorld();
-	FVector Location = GetActorLocation();
-	FVector ForwardVector = GetActorForwardVector();
-	FVector ExtentVector = FVector{ 20.f, 20.f, 20.f };
 
-	DRAW_SPHERE(Location);
-	DRAW_VECTOR(Location, Location + ForwardVector * 100.f);
-	DRAW_BOX(Location, ExtentVector);
+	int32 AvgInt = Avg<int32>(1, 3);
+	UE_LOG(LogTemp, Warning, TEXT("AVG of 1 and 3: %d"), AvgInt);
+
+	float AvgFloat = Avg<float>(3.653f, 4.234f);
+	UE_LOG(LogTemp, Warning, TEXT("AVG of 3.653, 4.234: %f"), AvgFloat);
+}
+
+float AItem::TransformedSin()
+{
+	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
+float AItem::TransformedCos()
+{
+	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
 }
 
 // Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	RunningTime += DeltaTime;
 
+	AddActorLocalRotation(FRotator(0.f, RotationSpeed * DeltaTime, 0.f));
+
+	DRAW_VECTOR_SINGLEFRAME(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
 }
 
